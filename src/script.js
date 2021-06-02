dac255f82df3f501907781af214dc90d2360dc46
Ice5050 <ice50505050@hotmail.com>
2021-06-02 15:37:12 +0700

    feat: donation

--- src/script.js
+++ src/script.js
@@ -229,3 +229,11 @@
 
   return
 })()
+
+function copyToClipboard(element) {
+  var $temp = $('<input>')
+  $('body').append($temp)
+  $temp.val($(element).text()).select()
+  document.execCommand('copy')
+  $temp.remove()
+}
