447c26d7b38b6fac3b30b363e3785b2959b7be1e
phwt.smwt <phwt.smwt@gmail.com>
2021-06-05 18:57:42 +0700

    Update Countdown.tsx

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,10 +1,19 @@
-import { Card } from 'react-bootstrap'
+import { Card, Col, Row } from 'react-bootstrap'
 
 const Countdown = () => {
   return (
     <Card className="h-100">
-      <Card.Body className="d-flex align-items-center justify-content-center">
-        <h4 className="m-0">Countdown</h4>
+      <Card.Body>
+        <Row className="h-100 d-flex align-items-center">
+          <Col md={4} className="text-center">
+            0.00 ($X.XX) <br />
+            <small className="text-muted">TWIN Locked</small>
+          </Col>
+          <Col md={8} className="text-center">
+            25 DAYS 09 HR 55 MIN 13 SEC <br />
+            <small className="text-muted">until rewards unlock</small>
+          </Col>
+        </Row>
       </Card.Body>
     </Card>
   )
