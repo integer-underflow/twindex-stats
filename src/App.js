425673ce299ec609edf43b722a7dd28182258f70
phwt.smwt <phwt.smwt@gmail.com>
2021-06-05 15:28:40 +0700

    Index layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import Header from './components/Header'
 
 const App = () => {
   return (
     <>
       <Header />
-      <Container>Content</Container>
+      <Container>
+        <Row>
+          <Col md="3">TWINDEX Price</Col>
+          <Col md="3">DOP Price</Col>
+          <Col md="6">TWINDEX Countdown</Col>
+          <Col md="12">LP Holdings</Col>
+          <Col md="6">Mint Positions</Col>
+          <Col md="6">Stock Price</Col>
+        </Row>
+      </Container>
     </>
   )
 }
